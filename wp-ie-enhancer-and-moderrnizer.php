e33757884356116bad3111035aafccf8fd5830d8
sLaNGjI <sLaNGjI@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-26 23:56:59 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-ie-enhancer-and-modernizer/trunk@377605 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-ie-enhancer-and-moderrnizer.php
+++ wp-ie-enhancer-and-moderrnizer.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: IE Enhancer and Modernizer
 Plugin URI: http://wordpress.org/extend/plugins/wp-ie-enhancer-and-modernizer/
-Description: IE <code>Enhancer and Modernizer</code> WordPress Surfing Expirience for old Microsoft Browsers | <a href="http://lcsn.net/donate/" title="Free Donation">Donate</a> | <a href="http://wordpress.org/extend/plugins/wp-overview-lite/" title="Show Dashboard Overview and Memory Load Usage on Footer">WP Overview?</a> | <a href="http://wordpress.org/extend/plugins/wp-admin-bar-removal/" title="Remove Admin Bar Frontend Backend and User Code">Admin Bar Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-total-deindexing/" title="Total DeIndexing WordPress from all Search Engines">WP DeIndexing?</a>
-Version: 2011.0427.0133
+Description: IE <code>Enhancer and Modernizer</code> WordPress Surfing Expirience for old Microsoft Browsers | <a href="http://lcsn.net/donate/" title="Free Donation">Donate</a> | <a href="http://wordpress.org/extend/plugins/wp-overview-lite/" title="Show Dashboard Overview and Memory Load Usage on Footer">WP Overview?</a> | <a href="http://wordpress.org/extend/plugins/wp-missed-schedule/" title="Fix Missed Schedule Future Posts Cron Job">WP Missed Schedule?</a> | <a href="http://wordpress.org/extend/plugins/wp-admin-bar-removal/" title="Remove Admin Bar Frontend Backend and User Code">Admin Bar Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-total-deindexing/" title="Total DeIndexing WordPress from all Search Engines">WP DeIndexing?</a>
+Version: 2011.0427.0156
 Author: sLa
 Author URI: http://wordpress.org/extend/plugins/profile/sla/
 Requires at least: 2.1
@@ -35,10 +35,10 @@ Tags: Make old Microsoft Internet Explorer behave like a standards-compliant bro
  * @package WP IE Enhancer and Modernizer
  * @subpackage WordPress PlugIn
  * @since 2.1.0
- * @version 2011.0427.0133
+ * @version 2011.0427.0156
  *
  * WordPress old Microsof IE Enhancer and Modernizer Surfing Expirience.
  */
 if(!function_exists('add_action')){header('Status 403 Forbidden');header('HTTP/1.0 403 Forbidden');header('HTTP/1.1 403 Forbidden');exit();}?><?php
-function wpieeam_log(){echo"<!--Plugin WP IE Enhancer and Modernizer 2011.0427.0133 Active-->";}add_action('wp_head','wpieeam_log');add_action('wp_footer','wpieeam_log')?><?php
+function wpieeam_log(){echo"<!--Plugin WP IE Enhancer and Modernizer 2011.0427.0156 Active-->";}add_action('wp_head','wpieeam_log');add_action('wp_footer','wpieeam_log')?><?php
 function wpieeam(){echo'<!--[if lt IE 9]><meta http-equiv="X-UA-Compatible" content="IE=8;IE=9;IE=10"><script src="http://ie7-js.googlecode.com/svn/trunk/lib/IE9.js">IE7_PNG_SUFFIX=".png";</script><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><script src="http://html5shim.googlecode.com/svn/trunk/html5-els.js"></script><![endif]-->';}add_action('wp_head','wpieeam')?>
