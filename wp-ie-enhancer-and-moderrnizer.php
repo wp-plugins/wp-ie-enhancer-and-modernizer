de98a81ca31f31859db18fc622d69948e74cfc87
sLaNGjI <sLaNGjI@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-21 01:14:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-ie-enhancer-and-modernizer/trunk@441144 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-ie-enhancer-and-moderrnizer.php
+++ wp-ie-enhancer-and-moderrnizer.php
@@ -3,18 +3,18 @@
 Plugin Name: IE Enhancer and Modernizer
 Plugin URI: http://wordpress.org/extend/plugins/wp-ie-enhancer-and-modernizer/
 Description: Make IE standards-compliant browser, fixes HTML/CSS issues, fixes transparent PNG under IE5 and IE6, shiv HTML5 elements, render to latest version. | <a href="http://lcsn.net/donate/" title="Free Donation">Donate</a> | <a href="http://wordpress.org/extend/plugins/wp-overview-lite/" title="Show Dashboard Overview and Memory Load Usage on Footer">WP Overview?</a> | <a href="http://wordpress.org/extend/plugins/wp-missed-schedule/" title="Fix Missed Schedule Future Posts Cron Job">WP Missed Schedule?</a> | <a href="http://wordpress.org/extend/plugins/wp-admin-bar-removal/" title="Remove Admin Bar Frontend Backend and User Code">Admin Bar Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-total-deindexing/" title="Total DeIndexing WordPress from all Search Engines">WP DeIndexing?</a>
-Version: 2011.0427.0454
+Version: 2011.0921.0310
 Author: sLa
 Author URI: http://wordpress.org/extend/plugins/profile/sla/
 Requires at least: 2.1
-Tested up to: 3.2
-License: GPLv2 or later
+Tested up to: 3.3
+License: GPLv3
 License URI: http://plugins.svn.wordpress.org/wp-ie-enhancer-and-modernizer/trunk/gfpl-2.0.txt
 Tags: ie, enhancer, modernizer, microsoft, browser, old, ie5.5, ie6, surfing, experience, wordpress, wp, html, css, fix, fixes, fixing, correct, corrections, html5, transparent, png, issues, elements, render, shiv, shim, ie7, ie7-js, js, htmlshiv, html5shiv, forces, page, latest, version, available, x-ua-compatible, x, ua, compatible, x-ua
 Excerpt: IE <code>Enhancer and Modernizer</code> WordPress Surfing Expirience for old Microsoft Browsers.
 Info: Make old Microsoft Internet Explorer behave like a standards-compliant browser: fixes many HTML and CSS issues, makes transparent PNG work correctly under IE5 and IE6, shiv to recognise and style the HTML5 elements, forces to render the page as the latest version available 8,9,10.
  *
- *  WP IE Enhancer and Modernizer - WordPress PlugIn
+ * WP IE Enhancer and Modernizer - WordPress PlugIn
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the [GNU General Public License](http://wordpress.org/about/gpl/)
@@ -37,10 +37,12 @@ Info: Make old Microsoft Internet Explorer behave like a standards-compliant bro
  * @package WP IE Enhancer and Modernizer
  * @subpackage WordPress PlugIn
  * @since 2.1.0
- * @version 2011.0427.0454
+ * @version 2011.0921.0310
  *
  * WordPress old Microsof IE Enhancer and Modernizer Surfing Expirience.
  */
 if(!function_exists('add_action')){header('Status 403 Forbidden');header('HTTP/1.0 403 Forbidden');header('HTTP/1.1 403 Forbidden');exit();}?><?php
-function wpieeam_log(){echo"<!--Plugin WP IE Enhancer and Modernizer 2011.0427.0454 Active-->";}add_action('wp_head','wpieeam_log');add_action('wp_footer','wpieeam_log')?><?php
-function wpieeam(){echo'<!--[if lt IE 9]><meta http-equiv="X-UA-Compatible" content="IE=8;IE=9;IE=10"><script src="http://ie7-js.googlecode.com/svn/trunk/lib/IE9.js">IE7_PNG_SUFFIX=".png";</script><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><script src="http://html5shim.googlecode.com/svn/trunk/html5-els.js"></script><![endif]-->';}add_action('wp_head','wpieeam')?>
+function wpieeam_log(){echo"\n<!--Plugin WP IE Enhancer and Modernizer 2011.0921.0310 Active-->\n";}add_action('wp_head','wpieeam_log');add_action('wp_footer','wpieeam_log')?><?php
+function wpieeam(){echo'<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<script src="http://ie7-js.googlecode.com/svn/trunk/lib/IE9.js">IE7_PNG_SUFFIX=".png";</script>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>';}add_action('wp_head','wpieeam')?>
