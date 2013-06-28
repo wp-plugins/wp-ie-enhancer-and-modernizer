ed252f99493ed560b36bb78637c296f6fcb9bf50
sLaNGjI <sLaNGjI@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-28 23:40:05 +0000

    Version 2013.0629.0138
    git-svn-id: https://plugins.svn.wordpress.org/wp-ie-enhancer-and-modernizer/trunk@733857 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,27 +1,47 @@
-=== √ IE Enhancer and Modernizer ===
+=== IE Enhancer and Modernizer ===
 Contributors: sLaNGjI
 Donate link: http://slangji.wordpress.com/donate/
-Tags: sLaNGjI
-Stable tag: 2013.0202.1848
+Tags: ie6, internet, explorer, internet-explorer, shiv, render, fix, edge, sla, slangji, wordpress, wp
+Stable tag: trunk
 Requires at least: 2.1
-Tested up to: 3.6
+Tested up to: 3.8
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
-&#9733;&#9733;&#9733; Make IE standards-compliant browser: fix HTML/CSS, fix transparent PNG IE5/6, shiv (X)HTML5 elements, render to latest version.
+make ie standards-compliant browser: fix html/css, fix transparent png ie6/5, shiv (x)html5 elements, render to latest version.
 == Description ==
-Make IE standards-compliant browser. Fixes HTML/CSS issues, fixes transparent PNG under IE5/IE6, shiv (X)HTML5 elements and render to latest version.
+make ie standards-compliant browser: fix html/css, fix transparent png ie6/5, shiv (x)html5 elements, render to latest version.
 
-For completely automatization of plugin, and no other action from the administrator, try on put it on `/mu-plugin/` directoy, and no other action will be required for activation and operation, starting from WordPress installation.
-
-Full Compatible with WordPress Versions from 2.1+ to 3.5+ add Preemptive Support for Coming Soon 3.6+ and Future 3.7+ ~ 3.8+ Releases. Ready to Single, Network Multisite installations, and old WPMU 2.6+ to 2.9+ (latest is 2.9.2) Multi Users. Also it is possible to work on old versions from 2.1+ to 2.5+ (latest is 2.5.1) and legacy 2.0+ branch, but the result is not guaranted. Run on Shared, Dedicated, Cloud and VPS Hosting, with high and low resources.
-
-Work under [GPLv2](http://www.gnu.org/licenses/gpl-2.0.html) or later License. Implement [GNU style](http://www.gnu.org/prep/standards/standards.html) coding standard indentation.
-`
-No need to delete anything from hosting space when deactivate!
-No need to delete anything from hosting space when deleted!
-No need to delete anything from the database when deactivate!
-No need to delete anything from the database when deleted!
-No need to delete anything from the wp_option when deactivate!
-No need to delete anything from the wp_option when deleted!
-Not need other actions except installing or uninstall it!
-`
+Make IE standards-compliant Browser: Enhancer and Modernizer WordPress Surfing Expirience. Fix and squish HTML/CSS issues, fix transparent PNG issues under IE5/IE6/IE7, shiv (X)HTML5 and CSS3 elements, render to latest version, add support for Google Chrome Frame, enable X-Content-Type, X-Frame and X-XSS protection, disable Image Toolbar.
+== Installation ==
+= Plugin Installation =
+1. Upload this plugin to the `/wp-content/plugins/` directory.
+1. Activate this plugin through the 'Plugins' menu in WordPress.
+1. The configuration of this Plugin is Automattic!
+== Frequently Asked Questions ==
+* Implement [GNU style](http://www.gnu.org/prep/standards/standards.html) coding standard indentation.
+* Meet detailed [Plugin Guidelines](http://wordpress.org/plugins/about/guidelines/) quality requirements.
+== Screenshots ==
+1. WP IE Enhancer and Modernizer Screenshot on WordPress
+== License ==
+Work under [GPLv2](http://www.gnu.org/licenses/gpl-2.0.html) or later License.
+== Changelog ==
+`All releases, prior latest stable, are deprecated, 
+and no longer supported on this project: is very 
+suggested upgrade to the latest build, always!`
+= Development Status =
+Developmental Code in Becoming!
+= 2013.0629.0138 =
+* Inremental Update [STABLE] Enhancer and Modernizer IE WordPress Surfing Expirience
+ * Please update as soon as possible!
+ * NEW Add Preemptive KeyTag Plugin Authenticity Support
+ * NEW Add Header and Footer Log
+ * UPDATED Security Rules
+ * UPDATED Removed non-ASCII characters from readme.txt and plugin header: Thanks to otto42
+ * BUMP Version  Build  Revision 
+= Common Rules =
+ * Work under [GPLv2](http://www.gnu.org/licenses/gpl-2.0.html) or later License.
+ * Implement [GNU style](http://www.gnu.org/prep/standards/standards.html) coding standard indentation.
+ * Meet detailed [Plugin Guidelines](http://wordpress.org/plugins/about/guidelines/) quality requirements.
+== Upgrade Notice ==
+= 2013.0629.0138 =
+Inremental Update [STABLE] Make IE standards-compliant Browser: Enhancer and Modernizer WordPress Surfing Expirience. Fix and squish HTML/CSS issues, fix transparent PNG issues under IE5/IE6/IE7, shiv (X)HTML5 and CSS3 elements, render to latest version, add support for Google Chrome Frame, enable X-Content-Type, X-Frame and X-XSS protection, disable Image Toolbar. The configuration of this Plugin is Automattic! Work under GPLv2 or later License. Implement GNU style coding standard indentation. Meet detailed Plugin Guidelines quality requirements.
--- wp-ie-enhancer-and-moderrnizer.php
+++ wp-ie-enhancer-and-moderrnizer.php
@@ -1,10 +1,11 @@
 <?php
+
 /*
-Plugin Name: √ WP IE Enhancer and Modernizer
+Plugin Name: WP IE Enhancer and Modernizer
 Plugin URI: http://slangji.wordpress.com/wp-ie-enhancer-and-modernizer/
-Description: Make IE standards-compliant browser. Fixes HTML/CSS issues, fixes transparent PNG under IE5/IE6, shiv (X)HTML5 elements and render to latest version. This Free Software Want You: please Contribute on it and Make your own Fantasy and Competence! (Code in Becoming) Work under <a href="http://www.gnu.org/licenses/gpl-2.0.html" title"GPLv2 or later License compatible">GPLv2</a> or later License. <a href="http://www.gnu.org/prep/standards/standards.html" title"GNU style indentation coding standard compatible">GNU style</a> indentation coding standard compatible. | <a href="http://slangji.wordpress.com/donate/" title="Free Donation">Donate</a> | <a href="http://slangji.wordpress.com/contact/" title="Send Me Bug and Suggestions">Contact</a> | <a href="http://profiles.wordpress.org/slangji" title="sLaNGjI's Profile @ WordPress.org">My Profile</a> | <a href="http://webscripts.softpedia.com/author/sLa-1869786722.html" title="sLa Developer Page @ SoftPedia.com">My SoftPedia</a> | <a href="http://slangji.wordpress.com/themes/" title="sLaNGjI's Custom Themes">My Themes</a> | <a href="http://wordpress.org/extend/plugins/wp-overview-lite/" title="Show Dashboard Overview and Footer Memory Load Usage">WP Overview?</a> | <a href="http://wordpress.org/extend/plugins/wp-missed-schedule/" title="Fix Missed Scheduled Future Posts Cron Job">WP Missed Schedule?</a> | <a href="http://wordpress.org/extend/plugins/wp-admin-bar-removal/" title="Remove Admin Bar Frontend Backend User Profile and Code">Admin Bar Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-admin-bar-node-removal/" title="Remove Admin Bar Frontend and Backend Node">Admin Bar Node Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-toolbar-removal/" title="Remove ToolBar Frontend Backend User Profile and Code">ToolBar Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-toolbar-node-removal/" title="Remove ToolBar Frontend and Backend Node">ToolBar Node Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-login-deindexing/" title="Total DeIndexing WordPress LogIn from all Search Engines">LogIn DeIndexing?</a> | <a href="http://wordpress.org/extend/plugins/wp-total-deindexing/" title="Total DeIndexing WordPress from all Search Engines">WP DeIndexing?</a> | <a href="http://wordpress.org/extend/plugins/wp-ie-enhancer-and-modernizer/" title="Enhancer and Modernizer IE Surfing Expirience">Enhancer IE Surfing?</a> | <a href="http://wordpress.org/extend/plugins/wp-wp-memory-db-indicator/" title="Memory Load Consumption db size Usage Indicator">Memory and db Indicator?</a>
-Version: 2013.0202.1848
-Author: sLa
+Description: Make IE standards-compliant browser. Fix HTML/CSS issues and transparent PNG under IE6/5, shiv (X)HTML5 elements, render latest version. Enhancer and Modernizer IE WordPress Surfing Expirience. The configuration of this Plugin is Automattic! Work under GPLv2 or later License. GNU style indentation coding standard compatible. Meet detailed guidelines quality requirements.
+Version: 2013.0629.0138
+Author: sLaNGjI
 Author URI: http://slangji.wordpress.com/
 Requires at least: 2.1
 Tested up to: 3.8
@@ -13,61 +14,9 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Indentation: GNU style coding standard
 Indentation URI: http://www.gnu.org/prep/standards/standards.html
  *
- * [IE Enhancer and Modernizer](http://wordpress.org/extend/plugins/wp-ie-enhancer-and-modernizer/)
+ * [IE Enhancer and Modernizer](http://wordpress.org/plugins/wp-ie-enhancer-and-modernizer/) Enhancer and Modernizer IE WordPress Surfing Expirience
  *
- * √ CONTACT
- *
- * [Contact](http://slangji.wordpress.com/contact/)
- *
- * √ DONATIONS
- *
- * [Donate](http://slangji.wordpress.com/donate/)
- *
- * √ PROJECTS
- *
- * [My Plugins](http://slangji.wordpress.com/plugins/)
- * [My Themes](http://slangji.wordpress.com/themes/)
- *
- * √ DEVELOPER
- *
- * [phpBB.com](http://phpbb.com/)
- * [SoftPedia.com](http://webscripts.softpedia.com/author/sLa-1869786722.html)
- * [WordPress.org](http://profiles.wordpress.org/slangji)
- *
- * √ REPOSITORIES
- *
- * [GitHub.com](https://github.com/slangji)
- *
- * √ WEBSITE
- *
- * [WordPress.com](http://slangji.wordpress.com/)
- *
- * √ PROFILES
- *
- * [bbPress.org](http://bbpress.org/forums/profile/slangji/)
- * [BuddyPress.org](http://buddypress.org/community/members/slangji/profile/public/)
- * [Gravatar.com](http://en.gravatar.com/slangji)
- *
- * Author URL [sLa](http://wordpress.org/extend/plugins/profile/sla) moved to [sLaNGjI](http://wordpress.org/extend/plugins/profile/slangji) update bookmark!
- *
- * √ POLLS
- *
- * [PollDaddy.com](http://slangji.polldaddy.com/)
- *
- * √ SUPPORT
- *
- * [WordPress.org](http://wordpress.org/support/profile/slangji)
- *
- * √ FOLLOW
- *
- * [LinkedIn](http://www.linkedin.com/in/slangjis)
- * [Twitter](https://twitter.com/sLanGjIs)
- * [FaceBook](https://www.facebook.com/sLaNGjI)
- * [Google+](https://plus.google.com/104369105810975562211)
- * [FeedBurner](http://feeds.feedburner.com/slangji)
- * [FriendFeed](http://friendfeed.com/slangjis)
- *
- * √ LICENSE
+ * Copyright (C) 2010-2013 [sLa NGjI's](http://slangji.wordpress.com/slangjis/) (email: <slangjis[at]gmail[dot]com>)
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the [GNU General Public License](http://wordpress.org/about/gpl/)
@@ -80,10 +29,11 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * along with this program; if not, see [GNU General Public Licenses](http://www.gnu.org/licenses/),
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * √ DISCLAIMER
+ * DISCLAIMER
  *
  * The license under which the WordPress software is released is the GPLv2 (or later) from the
  * Free Software Foundation. A copy of the license is included with every copy of WordPress.
@@ -96,53 +46,91 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
  *
  * The license for this software can be found on [Free Software Foundation](http://www.gnu.org/licenses/gpl-2.0.html) and as license.txt into this plugin package.
  *
- * √ GUIDELINES
- *
- * This software meet detailed [Plugin Guidelines](http://wordpress.org/extend/plugins/about/guidelines/) paragraphs 1,4,10,12,13,16,17 quality requirements.
  * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
  *
- * √ CODING
+ * THERMS
+ *
+ * This uses (or it parts) code derived from
+ *
+ * wp-header-footer-log.php by sLa <slangji[at]gmail[dot]com>
+ * Copyright (C) 2009-2013 [sLaNGjI](http://slangji.wordpress.com/) (email: <slangji[at]gmail[dot]com>)
+ *
+ * according to the terms of the GNU General Public License version 2 (or later)
+ *
+ * This wp-header-footer-log.php uses (or it parts) code derived from
+ *
+ * wp-footer-log.php by sLa <slangji[at]gmail[dot]com>
+ * Copyright (C) 2008-2013 [sLaNGjI](http://slangji.wordpress.com/) (email: <slangji[at]gmail[dot]com>)
+ *
+ * sLa2sLaNGjIs.php by sLa <slangji[at]gmail[dot]com>
+ * Copyright (C) 2009-2013 [sLaNGjI](http://slangji.wordpress.com/) (email: <slangji[at]gmail[dot]com>)
+ *
+ * according to the terms of the GNU General Public License version 2 (or later)
+ *
+ * According to the Terms of the GNU General Public License version 2 (or later) part of Copyright belongs to your own author and part belongs to their respective others authors:
+ *
+ * Copyright (C) 2008-2013 [sLaNGjI](http://slangji.wordpress.com/) (email: <slangji[at]gmail[dot]com>)
+ *
+ * VIOLATIONS
+ *
+ * [Violations of the GNU Licenses](http://www.gnu.org/licenses/gpl-violation.en.html)
+ * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
+ *
+ * GUIDELINES
+ *
+ * This software meet [Detailed Plugin Guidelines](http://wordpress.org/plugins/about/guidelines/) paragraphs 1,4,10,12,13,16,17 quality requirements.
+ * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
+ *
+ * CODING
  *
  * This software implement [GNU style](http://www.gnu.org/prep/standards/standards.html) coding standard indentation.
+ * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
  *
- * √ COPYRIGHT
+ * VALIDATION
  *
- * Part of Copyright © 2010-2013 belongs to [sLaNGjI's](http://en.gravatar.com/slangjis) (slangji[at]gmail[dot]com) and a portion to their respective owners.
+ * This readme.txt rocks. Seriously. Flying colors. It meet the specifications according to WordPress [Readme Validator](http://wordpress.org/plugins/about/validator/) directives.
+ * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
  */
-/**
- * @package IE Enhancer and Modernizer
- * @subpackage WordPress PlugIn
- * @description Enhancer and Modernizer IE WordPress Surfing Expirience.
- * @since 2.1.0
- * @version 2013.0202.1848
- * @status DEVELOPMENT Code in Becoming
- * @author sLa
- * @license GPLv2 or later
- * @indentation GNU style coding standard
- * @keytag s2gJ4RrV9t4i5GfQu4e0u82365E77P91XR7yyCSt18V4Fvf20eFm331938S5Ip16V
- * @checksum 9B93C1BD65FFFA993D9E03F5FAD58761
- */
+
+	/**
+	 * @package WP IE Enhancer and Modernizer
+	 * @subpackage WordPress PlugIn
+	 * @description Enhancer and Modernizer IE WordPress Surfing Expirience
+	 * @since 2.1.0
+	 * @tested 3.8.0
+	 * @version 2013.0629.0138
+	 * @status STABLE (trunk) release
+	 * @development Code in Becoming!
+	 * @author sLaNGjI
+	 * @license GPLv2 or later
+	 * @indentation GNU style coding standard
+	 * @keytag 74be16979710d4c4e7c6647856088456
+	 */
+
 	if ( !function_exists( 'add_action' ) )
 		{
+			header( 'HTTP/0.9 403 Forbidden' );
 			header( 'HTTP/1.0 403 Forbidden' );
 			header( 'HTTP/1.1 403 Forbidden' );
-			exit();
-		} //!function_exists( 'add_action' )
-
-	function wpieeam_log()
-		{
-			echo "\n<!--Plugin IE Enhancer and Modernizer 2012.0322.2238 Active - Key Tag: s2gJ4RrV9t4i5GfQu4e0u82365E77P91XR7yyCSt18V4Fvf20eFm331938S5Ip16V-->\n\n";
+			header( 'Status: 403 Forbidden' );
+			header( 'Connection: Close' );
+				exit();
 		}
-	add_action( 'wp_head', 'wpieeam_log' );
-	add_action( 'wp_footer', 'wpieeam_log' );
+
+	global $wp_version;
+
+	if ( $wp_version < 2.1 )
+		{
+			wp_die( __( 'This Plugin Requires WordPress 2.1+ or Greater: Activation Stopped!' ) );
+		}
 
 	function wpieeam()
 		{
 			echo '
-	<!--Plugin IE Enhancer and Modernizer Begin Code-->
+	<!--Start IE Enhancer and Modernizer Code-->
 	<meta http-equiv="X-Frame-Options" content="SAMEORIGIN">
 	<!--[if IE]>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<![endif]-->
 	<!--[if IE 6]>
 	<meta http-equiv="Imagetoolbar" content="no">
@@ -156,8 +144,16 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
 	<script src="http://ie7-js.googlecode.com/svn/trunk/lib/ie7-squish.js</script>
 	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
-	<!--/Plugin IE Enhancer and Modernizer End Code-->
+	<!--End IE Enhancer and Modernizer Code-->
 	';
 		}
 	add_action( 'wp_head', 'wpieeam' );
+
+	function wpieeam_header_footer_log()
+		{
+			echo "\n<!--Plugin IE Enhancer and Modernizer 2012.0322.2238 Active - Tag: ".md5(md5("".""))."-->\n";
+		}
+	add_action( 'wp_head', 'wpieeam_header_footer_log' );
+	add_action( 'wp_footer', 'wpieeam_header_footer_log' );
+
 ?>
